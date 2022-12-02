f5c0d1915dc25a47f957a3ad5ef5ca66c5283470
3merson <emersonjones95@gmail.com>
2022-12-02 14:13:51 -0500

    Simplifying code

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -2,24 +2,18 @@ import os
 
 os.chdir(os.path.dirname(__file__))
 
-elfs={}
-elf_counter=1
+list_of_calories = []
 line_num = 0
-with open ('input.txt', 'r') as fle:
+with open ('input.txt', 'r+') as fle:
+    fle.write('\n')
     for line in fle:
         if line == '\n':
-            elfs[f'Elf{elf_counter}'] = line_num
+            list_of_calories.append(line_num)
             line_num = 0
-            elf_counter+= 1
             continue
         else: 
             line_num = int(line.strip()) + line_num
-        
-list_of_calories = []
-
-for key in elfs:
-    list_of_calories.append(elfs[key])
-
+            
 list_of_calories.sort(reverse=True)
 
 #Max
